9db1e888ce9371733256d23fa206a72727bfc54e
Jessé Jaques <jessejaques29@gmail.com>
2025-11-12 00:03:40 +0000

    realizado nivel aventureiro

--- xadrez.c
+++ xadrez.c
@@ -5,20 +5,32 @@ int main() {
     /*Início do código com a movimentação da peça TORRE, antes de cada for
     coloquei um printf para separar cada bloco de movimentação do outro*/
 
-    printf("\nMovimento da peça Torre\n");
+    printf("\nMovimento da peça Torre:\n");
 for (int i = 0; i < 5; i++ ) {
-    printf("Movimento peça Torre: Direita\n");
+    printf("Direita\n");
 }
 
-    printf("\nMovimento da peça Bispo\n");
+    printf("\nMovimento da peça Bispo:\n");
 for (int i = 0; i < 5; i++ ) {
-    printf("Movimento peça Bispo na diagonal: Cima, Direita\n");
+    printf("Cima, Direita\n");
 }
 
-    printf("\nMovimento da peça Rainha\n");
+    printf("\nMovimento da peça Rainha:\n");
 for (int i = 0; i < 8; i++ ) {
-    printf("Movimento peça Rainha: Esquerda\n");
+    printf("Esquerda\n");
 }
+
+int movimentoCavalo = 1;
+
+    printf("\nMovimento da peça Cavalo:\n");
+while (movimentoCavalo--)
+{ for (int i = 0; i < 2; i++){
+    printf("Cima\n");
+
+}
+printf("Direita\n");
+}
+
 
 
 
